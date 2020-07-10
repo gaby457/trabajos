122d06d596e62898de34e98bcb45e1b498290967
gaby457 <68028095+gaby457@users.noreply.github.com>
2020-07-10 16:12:56 -0600

    Add files via upload

---
+++ evalucionmt.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author YANEZ
+ */
+public class evalucionmt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int M = 6, T = 1, K = - 10;
+ System.out.println(T>M);
+ System.out.println(T/K ==-5);
+ System.out.println(M+T==7);
+ System.out.println(M-T==7);
+ 
+    }
+    
+}
